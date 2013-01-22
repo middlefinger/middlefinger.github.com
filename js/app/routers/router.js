9b07459750e146760f46e99b68581e5e6ae03008
middlefinger <lefinger@yandex.com>
2013-01-22 18:47:50 +0200

    1

--- js/app/routers/router.js
+++ js/app/routers/router.js
@@ -5,14 +5,15 @@ app.routers.Workspace = Backbone.Router.extend({
 	},
 	initialize: function(){
 		console.log('init');
-		this.pagesHolder = jQuery('#pages');
-		app.collections.mainPages = new app.collections.pages;
-		app.ui.mainPages = new app.ui.mainApp({el: this.pagesHolder});
-		jQuery('#header').append(new app.ui.nav().$el);
-		this.pagesHolder.children('.page').eq(window.app.core.active).addClass('active');
+
 	},
 	main: function(){
 		console.log('main route');
+		
+		this.pagesHolder = jQuery('#pages');
+		app.collections.mainPages = new app.collections.pages;
+		app.ui.mainPages = new app.ui.mainApp({el: this.pagesHolder});
+		
 		if(window.app.core.active == null) {
 			app.ui.mainPages.openPage(0);
 			app.routers.mainWorkspace.navigate('page-0', {trigger: true});
