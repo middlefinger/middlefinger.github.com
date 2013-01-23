d842f92e3c1f234f9868fa3252e41b7a5aa58f55
middlefinger <lefinger@yandex.com>
2013-01-23 02:47:24 +0200

    dm

--- dm/lightbox/js/jquery.main.js
+++ dm/lightbox/js/jquery.main.js
@@ -3,15 +3,29 @@ jQuery(function(){
 });
 
 function initLb(){
-	jQuery('ul.image-list a').colorbox({
+	var cBox = jQuery('ul.image-list a').colorbox({
 		rel: 'gr',
 		previous: '&lt;',
 		next: '&gt;',
 		close: 'X',
-		onOpen:function(){ console.log('onOpen: colorbox is about to open', arguments); },
+		onOpen:function(){
+			jQuery('#colorbox').on('click', 'a.prev-work', prevColorBox);
+			jQuery('#colorbox').on('click', 'a.next-work', nextColorBox);
+			jQuery('#colorbox').on('click', 'a.return', closeColorBox);
+		},
 		onLoad:function(){ console.log('onLoad: colorbox has started to load the targeted content', arguments); },
 		onComplete:function(){ console.log('onComplete: colorbox has displayed the loaded content', arguments); },
 		onCleanup:function(){ console.log('onCleanup: colorbox has begun the close process', arguments); },
 		onClosed:function(){ console.log('onClosed: colorbox has completely closed', arguments); }
 	});
+	
+	function closeColorBox(){
+		cBox.colorbox.close();
+	}
+	function prevColorBox(){
+		cBox.colorbox.prev();
+	}
+	function nextColorBox(){
+		cBox.colorbox.next();
+	}
 }
