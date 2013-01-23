e79782c0c1c13c3ecc1684569c441872a6112a5a
middlefinger <lefinger@yandex.com>
2013-01-23 02:38:55 +0200

    dm

--- dm/lightbox/js/jquery.main.js
+++ dm/lightbox/js/jquery.main.js
@@ -7,6 +7,11 @@ function initLb(){
 		rel: 'gr',
 		previous: '&lt;',
 		next: '&gt;',
-		close: 'X'
+		close: 'X',
+		onOpen:function(){ console.log('onOpen: colorbox is about to open'); },
+		onLoad:function(){ console.log('onLoad: colorbox has started to load the targeted content'); },
+		onComplete:function(){ console.log('onComplete: colorbox has displayed the loaded content'); },
+		onCleanup:function(){ console.log('onCleanup: colorbox has begun the close process'); },
+		onClosed:function(){ console.log('onClosed: colorbox has completely closed'); }
 	});
 }
