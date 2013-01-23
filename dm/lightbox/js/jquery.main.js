702433f5ae131f815023b204bfb2e1f9096cc110
middlefinger <lefinger@yandex.com>
2013-01-23 02:41:34 +0200

    dm

--- dm/lightbox/js/jquery.main.js
+++ dm/lightbox/js/jquery.main.js
@@ -8,10 +8,10 @@ function initLb(){
 		previous: '&lt;',
 		next: '&gt;',
 		close: 'X',
-		onOpen:function(){ console.log('onOpen: colorbox is about to open'); },
-		onLoad:function(){ console.log('onLoad: colorbox has started to load the targeted content'); },
-		onComplete:function(){ console.log('onComplete: colorbox has displayed the loaded content'); },
-		onCleanup:function(){ console.log('onCleanup: colorbox has begun the close process'); },
-		onClosed:function(){ console.log('onClosed: colorbox has completely closed'); }
+		onOpen:function(){ console.log('onOpen: colorbox is about to open', arguments); },
+		onLoad:function(){ console.log('onLoad: colorbox has started to load the targeted content', arguments); },
+		onComplete:function(){ console.log('onComplete: colorbox has displayed the loaded content', arguments); },
+		onCleanup:function(){ console.log('onCleanup: colorbox has begun the close process', arguments); },
+		onClosed:function(){ console.log('onClosed: colorbox has completely closed', arguments); }
 	});
 }
